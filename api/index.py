b1bc35a6cbde52bffbfe19ff8534f54047645c4a
Abdullah Sajjad Saeed Minhas <aminhas1996@gmail.com>
2026-04-13 01:01:51 +0100

    Update Flask app to use custom template directory

--- api/index.py
+++ api/index.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# Path to templates one level up
+template_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "templates")
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route("/")
 def home():
-    return render_template("../templates/index.html", name="World")
+    return render_template("index.html", name="World")
